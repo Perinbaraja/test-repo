f9a24c51c0f89d969795630cf088c8b5e58ae8b2
perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2022-06-30 12:43:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.json({ message: "test" }));
+app.get("/", (req, res) => res.json({ message: "raja" }));
 app.listen(process.env.PORT || 80);
